06c04de3ab644521eae37f56ac4683bf2e97179c
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2020-06-16 17:57:09 -0300

    styled login

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const LOGIN = "LOGIN";
+const REGISTER = "REGISTER";
 
 function SignIn() {
-  return <h1>Sign In</h1>
+  const [typeForm, setTypeForm] = useState(LOGIN);
+
+  const isLogin = typeForm === LOGIN;
+  const isRegister = typeForm === REGISTER;
+
+  return (
+    <>
+      <div className='titleArea'>
+        <h1>Diário de Treino</h1>
+        <h4>Crie, edite e gerencie seus treinos</h4>
+      </div>
+      <div className='formArea'>
+        {/* <img alt='logo' /> */}
+
+        {isLogin &&
+          <form>
+            <input type='email' placeholder='Seu e-mail' />
+            <input type='password' placeholder='Sua senha' />
+
+            <button className='primary' type='submit'>Entrar</button>
+            <button className='secundary' type='button' onClick={() => setTypeForm(REGISTER)}>
+              Não tenho conta
+          </button>
+          </form>
+        }
+
+        {isRegister &&
+          <form>
+            <input placeholder='Seu nome' />
+            <input type='email' placeholder='Seu e-mail' />
+
+            <input type='password' placeholder='Sua senha' />
+            <input type='password' placeholder='Confirme sua senha' />
+
+            <button className='primary' type='submit'>Criar conta</button>
+            <button className='secundary' type='button' onClick={() => setTypeForm(LOGIN)}>
+              Já possuo uma conta
+          </button>
+          </form>
+        }
+      </div>
+    </>
+  )
 }
 
 export default SignIn;
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,7 +1,78 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #292929;
   color: #fff;
+  display: flex;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  .titleArea{
+    margin: 40px;
+    h1{
+      font-size: 50px;
+      color: #EE4540;
+    }
+    h4{
+      font-size: 28px;
+      margin-top: 15px;
+    }
+  }
+
+  .formArea{
+    display: flex;
+    flex-direction: column;
+    flex: 1 1;
+    justify-content: center;
+    align-items: center;
+
+    form{
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      max-width: 300px;
+      margin-top: 30px;
+      input{
+        background: rgba(255, 255, 255, 0.1);
+        border: 0;
+        border-radius: 4px;
+        height: 44px;
+        padding: 0 15px;
+        margin-bottom: 10px;
+        color: #fff;
+
+        &::placeholder{
+          color: rgba(255, 255, 255, 0.7);
+        }
+      }
+      button{
+        margin: 5px 0 0;
+        height: 44px;
+        font-weight: bold;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        color: #fff;
+        transition: background 0.2s;
+      }
+      .primary{
+        background: #EE4540;
+        &:hover{
+          background: ${darken(0.05, '#ee4540')};
+        }
+      }
+      .secundary{
+        background: #292929;
+        &:hover{
+          background: ${darken(0.05, '#292929')};
+        }
+      }
+    }
+  }
 `;
