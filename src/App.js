ebf8faa42f25b3b20fd995e48c3daeb82efa17dd
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2020-07-05 10:41:31 -0300

    fix react router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ToastContainer } from "react-toastify";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
+import { Router, BrowserRouter } from "react-router-dom";
 
 import "./config/ReactotronConfig";
 import Routes from "./routes/index";
@@ -16,11 +16,11 @@ function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Router history={history}>
+        <BrowserRouter history={history}>
           <Routes />
           <GlobalStyle />
           <ToastContainer autoClose={3000} />
-        </Router>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   );
